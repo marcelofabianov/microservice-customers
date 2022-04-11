abe977bd149d9d641ad037dc569520702de9dd3e
Marcelo Fabiano <marcelofabianov@gmail.com>
2022-04-11 19:01:40 -0300

    fix: EditAccountController

--- modules/Accounts/Http/Controllers/EditAccountController.php
+++ modules/Accounts/Http/Controllers/EditAccountController.php
@@ -5,16 +5,16 @@ namespace Modules\Accounts\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Modules\Accounts\Data\Models\Account;
-use Modules\Accounts\Http\Requests\EditContactsRequest;
+use Modules\Accounts\Http\Requests\EditAccountRequest;
 
 class EditAccountController extends Controller
 {
     /**
-     * @param EditContactsRequest $request
+     * @param EditAccountRequest $request
      * @param int $id
      * @return JsonResponse
      */
-    public function handle(EditContactsRequest $request, int $id): JsonResponse
+    public function handle(EditAccountRequest $request, int $id): JsonResponse
     {
         $account = Account::findOrFail($id);
         $account->fill($request->all());
