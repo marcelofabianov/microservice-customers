2c385eb948888756a8ed1690a7b236463db4f682
Marcelo Fabiano <marcelo.fabiano@noufiscal.com.br>
2022-04-12 16:55:40 -0300

    test: find_account_passing_links_parameter

--- tests/Feature/Accounts/FindAccountTest.php
+++ tests/Feature/Accounts/FindAccountTest.php
@@ -77,4 +77,34 @@ class FindAccountTest extends TestCase
 
         $this->assertEquals($expected, $actual->json());
     }
+
+    /**
+     * @test
+     */
+    public function find_account_passing_links_parameter()
+    {
+        $account = Account::factory()->create();
+
+        $actual = $this->get(env('API_URL').'/accounts/'.$account->id.'?links=true', $this->getHeadersAuthorization());
+
+        $expected = [
+            'id' => $account->id,
+            'document' => $account->document,
+            'name' => $account->name,
+            'address' => $account->address,
+            'district' => $account->district,
+            'city' => $account->city,
+            'complement' => $account->complement,
+            'status' => $account->status->value,
+            'createdAt' => Carbon::parse($account->created_at)->toIso8601String(),
+            'updatedAt' => Carbon::parse($account->updated_at)->toIso8601String(),
+            'links' => [
+                'self' => route('api.accounts.find', $account->id)
+            ]
+        ];
+
+        $actual->assertOk();
+
+        $this->assertEquals($expected, $actual->json());
+    }
 }
