2ed7c3b58df7e6e55488d8fd4e596485c5411eb2
Marcelo Fabiano <marcelo.fabiano@noufiscal.com.br>
2022-04-12 16:03:52 -0300

    test: creating list_of_accounts_that_contain_status_parameter

--- modules/Accounts/Http/Controllers/AccountsController.php
+++ modules/Accounts/Http/Controllers/AccountsController.php
@@ -4,6 +4,7 @@ namespace Modules\Accounts\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Modules\Accounts\Data\Enums\AccountStatusEnum;
 use Modules\Accounts\Data\Repositories\AccountRepository;
 use Modules\Accounts\Http\Resources\AccountCollection;
 
@@ -16,7 +17,10 @@ class AccountsController extends Controller
     public function handle(Request $request): AccountCollection
     {
         $repository = new AccountRepository();
-        $accounts = $repository->accounts($request->get('status'));
+
+        $accounts = $repository->accounts(
+            $request->has('status') ? AccountStatusEnum::from($request->get('status')) : null
+        );
 
         $perPage = $request->get('per_page', env('SIMPLE_PAGINATE_PER_PAGE'));
 
--- tests/Feature/Accounts/AccountsTest.php
+++ tests/Feature/Accounts/AccountsTest.php
@@ -60,4 +60,45 @@ class AccountsTest extends TestCase
 
         $this->assertEquals($expected, $actual->json());
     }
+
+    public function list_of_accounts_that_contain_status_parameter()
+    {
+        $account =  Account::factory()->create();
+
+        $actual = $this->get(env('API_URL').'/accounts?status=2', $this->getHeadersAuthorization());
+
+        $expected = [
+            'data' => [
+                [
+                    'id' => $account->id,
+                    'document' => $account->document,
+                    'name' => $account->name,
+                    'address' => $account->address,
+                    'district' => $account->district,
+                    'city' => $account->city,
+                    'complement' => $account->complement,
+                    'status' => $account->status->value,
+                    'createdAt' => Carbon::parse($account->created_at)->toIso8601String(),
+                    'updatedAt' => Carbon::parse($account->updated_at)->toIso8601String(),
+                ]
+            ],
+            'links' => [
+                'first' => route('api.accounts.index').'?page=1',
+                'last' => null,
+                'prev' => null,
+                'next' => null,
+            ],
+            'meta' => [
+                'current_page' => 1,
+                'from' => 1,
+                'path' => route('api.accounts.index'),
+                'per_page' => env('SIMPLE_PAGINATE_PER_PAGE', 20),
+                'to' => 1
+            ]
+        ];
+
+        $actual->assertOk();
+
+        $this->assertEquals($expected, $actual->json());
+    }
 }
