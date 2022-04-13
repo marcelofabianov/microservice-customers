6928616f0935e3b877d953b52dcec1a73f78752f
Marcelo Fabiano <marcelofabianov@gmail.com>
2022-04-13 11:21:10 -0300

    test: find_contact

--- modules/Contacts/Http/Controllers/FindContactController.php
+++ modules/Contacts/Http/Controllers/FindContactController.php
@@ -3,17 +3,17 @@
 namespace Modules\Contacts\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\JsonResponse;
 use Modules\Contacts\Data\Models\Contact;
+use Modules\Contacts\Http\Resources\ContactResource;
 
 class FindContactController extends Controller
 {
     /**
      * @param int $id
-     * @return JsonResponse
+     * @return ContactResource
      */
-    public function handle(int $id): JsonResponse
+    public function handle(int $id): ContactResource
     {
-        return response()->json(Contact::findOrFail($id));
+        return new ContactResource(Contact::findOrFail($id));
     }
 }
---
+++ tests/Feature/Contacts/FindContractTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Contacts;
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Modules\Contacts\Data\Models\Contact;
+use Tests\Feature\OAuth;
+use Tests\TestCase;
+
+class FindContractTest extends TestCase
+{
+    use RefreshDatabase;
+    use OAuth;
+    use WithFaker;
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function find_contact()
+    {
+        $contact = Contact::factory()->create();
+
+        $actual = $this->get(env('API_URL').'/contacts/'.$contact->id, $this->getHeadersAuthorization());
+
+        $expected = [
+            'id' => $contact->id,
+            'account_id' => $contact->account_id,
+            'description' => $contact->description,
+            'contact' => $contact->contact,
+            'type' => $contact->type->value,
+            'createdAt' => Carbon::parse($contact->created_at)->toIso8601String(),
+            'updatedAt' => Carbon::parse($contact->updated_at)->toIso8601String(),
+        ];
+
+        $actual->assertOk();
+
+        $this->assertEquals($expected, $actual->json());
+    }
+}
